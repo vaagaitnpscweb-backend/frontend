f2bf495c1c2ef8693daf76e6aa87e51c9ca409a4
vaagaitnpscweb-backend <vaagaitnpscweb@gmail.com>
2026-09-16 21:28:04 +0530

    Update MasterAdmin.jsx

--- src/AdminPages/MasterAdmin.jsx
+++ src/AdminPages/MasterAdmin.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../Styles/MasterAdmin.css';
+/*import '../Styles/MasterAdmin.css';*/
 import logoImg from '../assets/logoImg.jpeg';
 import { fetchTamilWord } from '../utils/tamilTransliterate';
 
